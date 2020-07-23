48e7f2b76081fea2a7bec2134fa0439134aac60d
Goce Mitevski <goce.mitevski@keitaro.com>
2020-07-23 22:30:45 +0200

    Refactor OneSchool component

--- src/components/OneSchool.js
+++ src/components/OneSchool.js
@@ -1,29 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import MunicipalityLink from './MunicipalityLink';
 
 const OneSchool = (props) => {
 
-  const [municipality, setMunicipality] = useState(props.data[2]);
-
-  useEffect(() => {
-    setMunicipality(municipality);
-  }, [municipality]);
-
   return (
-    <div className="row justify-content-center">
-      <div className="col-lg-10">
-        <aside className="card my-4">
-          <div className="card-body d-flex align-items-center justify-content-center">
-            <i className="text-danger far fa-4x fa-fw fa-sad-tear ml-4 mr-5"></i>
-            <div className="d-flex flex-column">
-              <h2 className="h4 card-title">
-                <span role="heading">Во <strong>{municipality}</strong> има само едно основно училиште</span>
-              </h2>
-              <p className="card-text">Очекуваме и се надеваме дека во иднина бројката на основни училишта во оваа општина значително ќе се зголеми.</p>
-            </div>
-          </div>
-        </aside>
+    <aside className={`card ${props.className}`}>
+      <div className="card-body d-flex align-items-center justify-content-center">
+        <i className="text-muted far fa-3x fa-fw fa-sad-tear ml-4 mr-5"></i>
+        <div className="d-flex flex-column">
+          <h4 className="h5 card-title">
+            <span role="heading">Во <MunicipalityLink {...props} municipality={props.data[2]} /> има само едно основно училиште</span>
+          </h4>
+          <p className="card-text">Очекуваме и се надеваме дека во иднина бројката на основни училишта во оваа општина значително ќе се зголеми.</p>
+        </div>
       </div>
-    </div>
+    </aside>
   );
 }
 
