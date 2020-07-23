b508fc390713e43345f36867a28824550b0d40a8
Goce Mitevski <goce.mitevski@keitaro.com>
2020-07-23 23:25:33 +0200

    Add NotFound component

---
+++ src/components/NotFound.js
@@ -0,0 +1,27 @@
+import React, {useEffect} from 'react';
+import { useLocation } from 'react-router-dom';
+import { pageTitle } from '../utils';
+
+const NotFound = (props) => {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    document.title = pageTitle(props.title);
+  }, [props]);
+
+  return (
+    <main className="container page py-5">
+      <header>
+        <h1 className="h3 d-flex align-items-center">
+          <i className="fa fas fa-2x fa-times-circle mr-3 text-danger"></i>
+          <span>{props.title}</span>
+        </h1>
+        <p>Извинете, но страницата <code>{location.pathname}</code> не постои или пак можеби некогаш постоела и била избришана.</p>
+        <p className="mb-0">Ве молам проверете дали случајно погрешивте при внесувањето.</p>
+      </header>
+    </main>
+  );
+}
+
+export default NotFound;
