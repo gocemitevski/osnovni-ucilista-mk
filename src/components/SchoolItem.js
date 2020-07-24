bdd06408a0aa632ba8be88d6199e5d63878688a5
Goce Mitevski <goce.mitevski@keitaro.com>
2020-07-25 00:23:41 +0200

    Truncate long email address buttons

--- src/components/SchoolItem.js
+++ src/components/SchoolItem.js
@@ -4,12 +4,10 @@ import SchoolItemLink from './SchoolItemLink';
 const Mail = (props) => {
 
   return (
-    <li className="list-inline-item mt-2">
-      <a className="btn btn-sm btn-success" href={`mailto:${props.props}`}>
-        <i className="fa fa-fw fa-envelope mr-2"></i>
-        <span>{props.props}</span>
-      </a>
-    </li>
+    <a className="nav-link my-1 mr-sm-2 px-3 py-2 btn-sm btn-success btn-truncate" title={props.props} href={`mailto:${props.props}`}>
+      <i className="fa fa-fw fa-envelope mr-2"></i>
+      <span>{props.props}</span>
+    </a>
   )
 }
 
@@ -18,7 +16,7 @@ const SchoolItem = (props) => {
   const emails = props.data[5].split(/[\s|,|;]/);
 
   return (
-    <article className="card mb-3" key={props.index}>
+    <article className="card" key={props.index}>
       <div className="row no-gutters">
         <div className="col-lg-6 p-4 d-flex">
           <h2 className="h5 mb-0 flex-fill">
@@ -31,7 +29,7 @@ const SchoolItem = (props) => {
               <dt>Адреса:</dt>
               <dd><address className="mb-0">{props.data[4]}<br />{props.data[2]}{props.data[7] && `, ${props.data[7]}`}</address></dd>
             </dl>
-            {emails.length > 0 && <ul className="list-inline mb-0">{emails.map((item, key) => <Mail key={key} props={item} />)}</ul>}
+            {emails.length > 0 && <div className="nav mt-3 mb-0">{emails.map((item, key) => <Mail key={key} props={item} />)}</div>}
           </div>
         </div>
       </div>
