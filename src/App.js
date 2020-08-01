f9b415b7188c25aede91ac312628637f9399c071
Goce Mitevski <goce.mitevski@keitaro.com>
2020-08-01 14:33:07 +0200

    Update Router basename

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ function App() {
   }, [skopjeSchoolsCount]);
 
   return (
-    <Router basename="/osnovni-ucilista-mk">
+    <Router basename={`${process.env.PUBLIC_URL}/osnovni-ucilista-mk`}>
       <NavBar routes={routes} data={data.records} />
       <Switch>
         {routes.map((item, key) => <Route key={key} exact={item.exact} path={item.path} render={(props) => <item.component {...props} scroll={scroll} setScroll={setScroll} title={item.title} setTitle={setTitle} data={data.records} municipalitiesSort={municipalitiesSort} skopjeTitle={skopjeTitle} skopjeSchoolsCount={skopjeSchoolsCount} />} />)}
