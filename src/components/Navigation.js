895f2a95ba80919b7b0083a44d78d9a8ebc6a328
Goce Mitevski <goce.mitevski@keitaro.com>
2020-08-01 13:57:24 +0200

    Update Navigation markup

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 const Navigation = (props) => {
 
   return (
-    <ul className="flex-shrink-0 navbar-nav ml-auto">
-      {props.routes.map((item, key) => item.path !== "/politika-za-privatnost" && <li key={key} className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to={item.path}>{item.title}</NavLink></li>)}
+    <ul className={`flex-shrink-lg-0 text-center navbar-nav nav-pills ml-lg-auto ${props.className}`}>
+      {props.routes.map((item, key) => <li key={key} className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to={item.path}>{item.title}</NavLink></li>)}
     </ul>
   )
 }
