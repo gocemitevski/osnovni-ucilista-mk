ceec53509c85da207e1257a9fbf73b7a0e4f86f5
Goce Mitevski <goce.mitevski@keitaro.com>
2020-08-01 14:58:13 +0200

    Update image src within the About component

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ const About = (props) => {
         <div className="col-lg-6 order-lg-2">
           <figure>
             <a href="/osnovni-uchilishta-r-s-makedonija-1200x627.png">
-              <img className="img-fluid" src="/osnovni-uchilishta-r-s-makedonija-1200x627.png" alt="" />
+              <img className="img-fluid" src={`${process.env.PUBLIC_URL}/osnovni-uchilishta-r-s-makedonija-1200x627.png`} alt="" />
             </a>
           </figure>
         </div>
