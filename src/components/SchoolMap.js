f14bfc7a6440da9f448978990c02e9f9fdd4acf1
Goce Mitevski <goce.mitevski@keitaro.com>
2020-08-01 23:04:57 +0200

    Disable map drag events

--- src/components/SchoolMap.js
+++ src/components/SchoolMap.js
@@ -5,7 +5,7 @@ import SchoolMapMarker from './SchoolMapMarker';
 const SchoolMap = (props) => {
 
   return (
-    <Map style={props.style} scrollWheelZoom={props.scrollWheelZoom} center={props.position} zoom={props.zoom}>
+    <Map style={props.style} scrollWheelZoom={props.scrollWheelZoom} dragging={false} center={props.position} zoom={props.zoom}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
