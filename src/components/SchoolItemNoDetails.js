ea557982c49fafb8c9ab7a372134dbb512dd5138
Goce Mitevski <goce.mitevski@keitaro.com>
2020-08-03 09:18:56 +0200

    Replace h2 with h3

--- src/components/SchoolItemNoDetails.js
+++ src/components/SchoolItemNoDetails.js
@@ -13,7 +13,7 @@ const SchoolItemNoDetails = (props) => {
     <Link onClick={() => { props.setScroll(props.scroll); props.setSchool(props.data); props.setPosition(props.data[6]) }} to={`/uchilishte/${encodeURIComponent(cleanName(transliterate(props.data[3] + ' ' + props.data[2])).toLowerCase())}`}>
       <article key={props.index} className="d-flex align-items-center">
         <i className="text-muted fa fa-chalkboard-teacher mx-2"></i>
-        <h2 className="h5 m-2">{props.data[3]}</h2>
+        <h3 className="h5 m-2">{props.data[3]}</h3>
       </article>
     </Link>
   );
