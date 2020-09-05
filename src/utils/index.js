5d156435b83b1ff5590caf40efbfa0bfad2fd49c
Goce Mitevski <goce.mitevski@keitaro.com>
2020-09-05 14:18:27 +0200

    Export socialLinkButtons

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,4 @@ export { cleanName } from './cleanName';
 export { pageTitle } from './pageTitle';
 export { pageDescription } from './pageDescription';
 export { singularPlural } from './singularPlural';
+export { socialLinkButtons } from './socialLinkButtons';
