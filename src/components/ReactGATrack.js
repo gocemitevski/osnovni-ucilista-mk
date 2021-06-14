41f893741398f294ab0546cf452d151e54601509
Goce Mitevski <goce.mitevski@keitaro.com>
2021-06-14 22:51:41 +0200

    Refactor ReactGATrack

--- src/components/ReactGATrack.js
+++ src/components/ReactGATrack.js
@@ -1,11 +1,10 @@
 import ReactGA from 'react-ga';
-import { pageTitle } from '../utils';
 import { Cookies } from "react-cookie-consent";
 
 const ReactGATrack = (props) => {
 
   if (Cookies.get("osnovniUcilistaMK") === 'true') {
-    ReactGA.set({ title: pageTitle(props.title) });
+    ReactGA.set({ title: props.title });
     props.location && ReactGA.pageview(props.location.pathname + props.location.search, props.title);
   }
   return null;
