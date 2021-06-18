58fcb093257f6f81ddeee326b8f840d7a73afde0
Goce Mitevski <goce.mitevski@keitaro.com>
2021-06-18 13:55:10 +0200

    Fix undefined being added to CSS classes

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
 
   return (
-    <div className={`card ${props.className}`}>
+    <div className={`card ${props.className || ''}`}>
       <div className="card-body text-muted">
         <span className={`value bg-${props.bgColor}`}>{props.value}</span>
         <span role="heading" className="text-uppercase flex-grow-1 my-1">{props.title}</span>
